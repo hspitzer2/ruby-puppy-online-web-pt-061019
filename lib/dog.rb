c06cf03b8dd6eb9584c34abf5b8000e69a2572c5
Hillary Spitzer <hspitzer2@gmail.com>
2019-07-18 18:04:49 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,28 +1,39 @@
+require 'pry'
+
 class Dog
 
-  @@all = []
-
   attr_accessor :name
-
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-  def self.clear_all
-    all.clear
-    @@all
-  end
-
-
-  def self.print_all
-    all.print each do |name| puts "#{name}!"
+   @@all =[]
+    def initialize(name)
+      @name = name
+      @@all << self
     end
-    print_all
-   end
 
+    def name
+      @name
+    end
+
+    def self.all
+      @@all
+    end
+
+    def self.clear_all
+      all.clear
+      @@all
+    end
+
+    # Person.all.each do |person|
+    #   puts "#{person.name}"
+    # end
+
+    # def self.print_all
+    #   self.name.each do |name| name.to_s
+    #   puts dog.name
+    # end
+
+    def self.print_all
+      @@all.each do |dog|
+        puts dog.name
+      end
+    end
 end
